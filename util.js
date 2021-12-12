ea6f3b3c5f43521a25e4af02edf0cf246d933fad
covert-encryption <93869732+covert-encryption@users.noreply.github.com>
2021-12-12 15:53:53 +0000

    Update util.js

--- util.js
+++ util.js
@@ -1,2 +1,5 @@
 // String to ArrayBuffer conversion with Unicode normalisation
 export const encode = str => new TextEncoder().encode(str.normalize('NFKC'))
+
+// Randomize padding value (bytes) using exponential distributen and the given preferred mean size
+const randpad = prefsize => Math.round(-prefsize * Math.log(1 - Math.random()))
