a5074e6e0689303c210e2f154e227f280f826ea3
covert-encryption <93869732+covert-encryption@users.noreply.github.com>
2021-12-12 00:05:50 +0000

    Fix Argon2 hash lengths

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const argon2 = async (pw, salt, cost) => {
+const argon2 = async (hashLen, pw, salt, cost) => {
   const res = await new Promise((resolve, reject) => {
     const argon2 = new Worker('./argon2-worker.js')
     argon2.onmessage = resolve
@@ -8,7 +8,7 @@ const argon2 = async (pw, salt, cost) => {
       salt: salt,
       time: cost,
       mem: 262144,
-      hashLen: 16,
+      hashLen,
       parallelism: 1,
       type: 2,
     })
@@ -16,8 +16,8 @@ const argon2 = async (pw, salt, cost) => {
   return res.data
 }
 
-const pwhash = async pw => argon2(pw, encode("covertpassphrase"), 8 << Math.max(0, 12 - pw.length))
-const pwauthkey = async (pwhash, nonce) => argon2(nonce, pwhash, 2)
+const pwhash = async pw => argon2(16, pw, encode("covertpassphrase"), 8 << Math.max(0, 12 - pw.length))
+const pwauthkey = async (pwhash, nonce) => argon2(32, nonce, pwhash, 2)
 
 // String to ArrayBuffer conversion with Unicode normalisation
 const encode = str => new TextEncoder().encode(str.normalize('NFKC'))
