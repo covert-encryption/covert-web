de4184ee2942d388989745f2781898be0c46f6a7
covert-encryption <93869732+covert-encryption@users.noreply.github.com>
2021-12-13 12:55:35 +0000

    Update util.js

--- util.js
+++ util.js
@@ -2,6 +2,19 @@
 export const encode = str => new TextEncoder().encode(str.normalize('NFKC'))
 
 
+// Generate a secure random number [0, 1)
+export const random = () => {
+  const r = new Uint32Array(2)
+  crypto.getRandomValues(r)
+  r[0] &= ~1  // Keep it under 1.0
+  return r[0] * 2**-64 + r[1] * 2**-32
+}
+
+
+// Randomize padding value (bytes) using exponential distributen and the given preferred mean size
+const randpad = prefsize => Math.round(-prefsize * Math.log(1 - random()))
+
+
 // Calculate random padding size in bytes as (roughly) proportion p of total size.
 export const random_padding = (total, p=0.05) => {
   if (!p) return 0
@@ -12,17 +25,4 @@ export const random_padding = (total, p=0.05) => {
   const prefsize = 1 + p * 200 + p * .7e8 * Math.log2(1 + 1e-8 * Math.max(low, total))
   // Apply pad-to-fixed-size for very short messages plus random padding
   return padfixed + randpad(prefsize)
-}
-
-
-// Randomize padding value (bytes) using exponential distributen and the given preferred mean size
-const randpad = prefsize => Math.round(-prefsize * Math.log(1 - random()))
-
-
-// Generate a secure random number [0, 1)
-export const random = () => {
-  const r = new Uint32Array(2)
-  crypto.getRandomValues(r)
-  r[0] &= ~1  // Keep it under 1.0
-  return r[0] * 2**-64 + r[1] * 2**-32
 }
