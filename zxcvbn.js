20482bd331d2d5a4108b7d52ea06be926073cc27
sh-dv <44544201+sh-dv@users.noreply.github.com>
2022-01-03 01:19:17 +0200

    use the maintained zxcvbn-ts instead of the old dropbox one

---
+++ zxcvbn.js
@@ -0,0 +1,19 @@
+import { zxcvbn, ZxcvbnOptions } from "@zxcvbn-ts/core";
+import zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
+import zxcvbnEnPackage from "@zxcvbn-ts/language-en";
+
+const options = {
+  translations: zxcvbnEnPackage.translations,
+  graphs: zxcvbnCommonPackage.adjacencyGraphs,
+  dictionary: {
+    ...zxcvbnCommonPackage.dictionary,
+    ...zxcvbnEnPackage.dictionary,
+  },
+};
+
+ZxcvbnOptions.setOptions(options);
+
+
+export const estimate = (pw) => {
+    return zxcvbn(pw)
+}
